c9d59553ce5265f9da65db4da102cd665fbd7bbd
John McDonald <john@threatrix.io>
2023-06-22 20:52:44 -0500

    Update gtp.java

--- examples/java/gtp.java
+++ examples/java/gtp.java
@@ -25,3 +25,42 @@ public class Base64Encoder {
         System.out.println("Encoded String: " + encodedString);
     }
 }
+
+public class Base64Decoder {
+    private static final int[] BASE64_ALPHABET = new int[256];
+    static {
+        for (int i = 0; i < 64; i++) {
+            BASE64_ALPHABET["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".charAt(i)] = i;
+        }
+    }
+    public static String decode(String encodedString) {
+        StringBuilder decodedStringBuilder = new StringBuilder();
+        int paddingCount = 0;
+        int length = encodedString.length();
+        for (int i = length - 1; encodedString.charAt(i) == '='; i--) {
+            paddingCount++;
+        }
+        int block = 0;
+        int blockLength = 0;
+        for (int i = 0; i < length; i++) {
+            char c = encodedString.charAt(i);
+            if (c == '=')
+                continue;
+            int value = BASE64_ALPHABET[c];
+            block = (block << 6) | value;
+            blockLength += 6;
+            if (blockLength >= 8) {
+                blockLength -= 8;
+                int decodedByte = (block >> blockLength) & 0xFF;
+                decodedStringBuilder.append((char) decodedByte);
+            }
+        }
+        return decodedStringBuilder.toString();
+    }
+    public static void main(String[] args) {
+        String encodedString = "SGVsbG8sIFdvcmxkIQ==";
+        System.out.println("Encoded String: " + encodedString);
+        String decodedString = decode(encodedString);
+        System.out.println("Decoded String: " + decodedString);
+    }
+}
