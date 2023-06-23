c909dbff834605adf74eb52b745d0a44b6846144
John McDonald <john@threatrix.io>
2023-06-23 12:41:14 -0500

    Update gpt5.java

--- examples/java/gpt5.java
+++ examples/java/gpt5.java
@@ -1,23 +1,24 @@
 void decode(final byte[] input, int inPos, final int inAvail, final Context context) {
-        if(context.eof) {
+        if (context.eof) {
             return;
         }
-        if(inAvail < 0) {
+        if (inAvail < 0) {
             context.eof = true;
         }
-        for(int i = 0; i < inAvail; i++) {
+        for (int i = 0; i < inAvail; i++) {
             final byte[] buffer = ensureBufferSize(decodeSize, context);
             final byte b = input[inPos++];
-            if(b == PAD) {
+            if (b == PAD) {
+                // We're done.
                 context.eof = true;
                 break;
             }
-            if(b >= 0 && b < DECODE_TABLE.length) {
+            if (b >= 0 && b < DECODE_TABLE.length) {
                 final int result = DECODE_TABLE[b];
-                if(result >= 0) {
+                if (result >= 0) {
                     context.modulus = (context.modulus + 1) % BYTES_PER_UNENCODED_BLOCK;
                     context.ibitWorkArea = (context.ibitWorkArea << BITS_PER_ENCODED_BYTE) + result;
-                    if(context.modulus == 0) {
+                    if (context.modulus == 0) {
                         buffer[context.pos++] = (byte) ((context.ibitWorkArea >> 16) & MASK_8BITS);
                         buffer[context.pos++] = (byte) ((context.ibitWorkArea >> 8) & MASK_8BITS);
                         buffer[context.pos++] = (byte) (context.ibitWorkArea & MASK_8BITS);
@@ -25,18 +26,4 @@ void decode(final byte[] input, int inPos, final int inAvail, final Context cont
                 }
             }
         }
-        
-        if(context.eof && context.modulus >= 2) {
-            final byte[] buffer = ensureBufferSize(decodeSize, context);
-            switch(context.modulus) {
-                case 2: // 12 bits = 8 + 4
-                    buffer[context.pos++] = (byte) ((context.ibitWorkArea >> 2) & MASK_8BITS);
-                    break;
-                case 3: // 18 bits = 8 + 8 + 2
-                    buffer[context.pos++] = (byte) ((context.ibitWorkArea >> 10) & MASK_8BITS);
-                    buffer[context.pos++] = (byte) ((context.ibitWorkArea >> 2) & MASK_8BITS);
-                    break;
-                default:
-                    throw new IllegalStateException("Impossible modulus " + context.modulus);
-            }
-        }
+    }
